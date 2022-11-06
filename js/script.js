992ce9218507a99f4f91f677e4ed176d639ac2ea
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2022-11-06 20:40:14 -0300

    jogo finalizado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -12,12 +13,21 @@ const jump = () => {
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 120) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        pipe.style.animation = 'none';
+        pipe.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '70px'
+        mario.style.marginLeft = '50px'
     }
 
-}, 10)
+}, 10);
 
 document.addEventListener('keydown', jump);
